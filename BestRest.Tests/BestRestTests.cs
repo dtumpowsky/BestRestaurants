df9f59f84c6ef785f123e3edaa7dcf89d2bbfeeb
David Tumpowsky and Randy Weyer <david@>
2018-05-02 10:33:36 -0700

    Initial Commit

---
+++ BestRest.Tests/BestRestTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BestRest;
+
+namespace BestRest.Tests
+{
+  // [TestClass]
+  // public class BestRestTest
+  // {
+  //   [TestMethod]
+  //   public void SortTwoElements_True()
+  //   {
+  //     int[] expectedArray = new int[] {1,7};
+  //     int[] UnsortedArray = new int[] {7,1};
+  //     BestRest testBestRest = new BestRest();
+  //     CollectionAssert.AreEqual(expectedArray, testBestRest.SortArray(UnsortedArray));
+  //   }
+  //
+  // }
+}
---
+++ BestRest/Models/BestRest.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BestRest
+{
+  public class BestRest
+  {
+    public int[] SortArray(int[] UnsortedArray)
+    {
+      
+    }
+
+
+  // End of Class
+ }
+}
